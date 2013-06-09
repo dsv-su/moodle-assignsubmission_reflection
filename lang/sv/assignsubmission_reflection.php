be8a0b6539fc2138d074c3c235d7d69876a912d0
Pavel Sokolov <pavel.m.sokolov@gmail.com>
2013-06-09 16:53:51 +0200

    SV lang file

---
+++ lang/sv/assignsubmission_reflection.php
@@ -0,0 +1,44 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'assignsubmission_reflection', language 'sv'
+ *
+ * @package assignsubmission_reflection
+ * @copyright 2013 Department of Computer and System Sciences,
+ *					Stockholm University  {@link http://dsv.su.se}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['cannotaddreflection'] = 'Du har redan lämnat in din reflektion';
+$string['commentmissing'] = 'Det finns reflektioner du inte kommenterat';
+$string['comments'] = 'Kommentarer: ';
+$string['default'] = 'Aktiverad som standard';
+$string['default_help'] = 'Om aktiverat kommer denna inställning gälla som standard för alla framtida inlämningar.';
+$string['enabled'] = 'Reflektion';
+$string['enabled_help'] = 'Om aktiverat kan studenterna bearbeta reflektionerna.';
+$string['forumintro'] = '<p>För instruktioner, gå till den ursprungliga uppgiften {$a->href} {$a->name}</a>.</p>';
+$string['forumlink'] = '<p>Alla gruppmedlemmar har nu skickat in sina reflektioner. Du skall nu skicka minst en kommentar till varje reflektion innan du är klar för betygsättning. Fortsätt genom att klicka på "ändra inlämning" knappen eller via denna länk: {$a} Go to Forum</a>.</p>';
+$string['incomplete'] = 'Väntar på att {$a->currentusers} av {$a->neededusers} ska lämna in sina reflektioner';
+$string['pluginname'] = 'Reflektion';
+$string['postmade'] = 'Reflektion inlämnad: ';
+$string['reflection'] = 'Reflektionsinlämning';
+$string['reflectionadded'] = 'Din reflektion har lagts till';
+$string['reflectioncancelled'] = 'Tillbaks till uppgiften';
+$string['reflectionfilename'] = 'reflektion.html';
+$string['students'] = 'Antal studenter i en grupp';
+$string['students_help'] = 'Denna inställning bestämmer antalet studenter i grupperna';
+$string['waitingname'] = 'Väntar på grupp för {$a}';
